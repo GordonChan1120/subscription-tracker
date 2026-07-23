71eff718da25000617a10cf7356e25b394791855
Chan Gordon <gordonchan015@gmail.com>
2026-07-24 00:53:53 +0800

    Implemented electron

---
+++ electron/main.ts
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  })
+
+   win.removeMenu() 
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'))
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
